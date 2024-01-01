8b7858f4efabbfcb9c22e0fcb723e82c96d690b9
Esmaeeil enani <esmaeeilenani@gmail.com>
2024-01-01 14:04:47 +0300

    adding restaurant domain events

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordring/system/restaurant/service/domain/event/OrderApprovedEvent.java
@@ -0,0 +1,30 @@
+package com.food.ordring.system.restaurant.service.domain.event;
+
+
+import com.food.ordring.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordring.system.domain.valueobject.RestaurantId;
+import com.food.ordring.system.restaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderApprovedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderApprovedEvent> publisher;
+
+    public OrderApprovedEvent(OrderApproval orderApproval,
+                              RestaurantId restaurantId,
+                              List<String> failureMessages,
+                              ZonedDateTime createdAt,
+                              DomainEventPublisher<OrderApprovedEvent> publisher
+    ) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+        this.publisher = publisher;
+    }
+
+
+    @Override
+    public void fire() {
+        this.publisher.publish(this);
+    }
+}
---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordring/system/restaurant/service/domain/event/OrderRejectedEvent.java
@@ -0,0 +1,30 @@
+package com.food.ordring.system.restaurant.service.domain.event;
+
+
+import com.food.ordring.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordring.system.domain.valueobject.RestaurantId;
+import com.food.ordring.system.restaurant.service.domain.entity.OrderApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderRejectedEvent extends OrderApprovalEvent {
+
+    private final DomainEventPublisher<OrderRejectedEvent> publisher;
+
+    public OrderRejectedEvent(OrderApproval orderApproval,
+                              RestaurantId restaurantId,
+                              List<String> failureMessages,
+                              ZonedDateTime createdAt,
+                              DomainEventPublisher<OrderRejectedEvent> publisher
+    ) {
+        super(orderApproval, restaurantId, failureMessages, createdAt);
+        this.publisher = publisher;
+    }
+
+
+    @Override
+    public void fire() {
+        this.publisher.publish(this);
+    }
+}
