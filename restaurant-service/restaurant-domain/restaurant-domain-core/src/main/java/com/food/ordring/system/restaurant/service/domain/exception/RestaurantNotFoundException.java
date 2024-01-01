d654621890a8fbc1481bb914ca792f4df8deb203
Esmaeeil enani <esmaeeilenani@gmail.com>
2024-01-01 13:55:22 +0300

    adding restaurant domain exceptions

---
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordring/system/restaurant/service/domain/exception/RestaurantNotFoundException.java
@@ -0,0 +1,13 @@
+package com.food.ordring.system.restaurant.service.domain.exception;
+
+import com.food.ordring.system.domain.exception.DomainException;
+
+public class RestaurantNotFoundException extends DomainException {
+    public RestaurantNotFoundException(String message) {
+        super(message);
+    }
+
+    public RestaurantNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
